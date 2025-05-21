eba4699ab8213b0f30f9e166f9770ca795f2dffa
Mohammed Hassan <mh9989700@gmail.com>
2025-05-21 23:09:20 +0300

    creating my project

---
+++ lib/feature/Splash_screen/Splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nft/app/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
